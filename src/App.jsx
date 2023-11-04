924b01103de401a29bb3002c6bcf33fb05d2d83a
Andhika Prasetya <andhikapr081@gmail.com>
2023-11-04 10:24:36 +0700

    feat: pass the param and searchParam through url

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 // pages
-import Product from './pages/Product';
-import Homepage from './pages/Homepage';
-import Pricing from './pages/Pricing';
-import Login from './pages/Login';
-import AppLayout from './pages/AppLayout';
-import PageNotFound from './pages/PageNotFound';
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 
 // component
-import CityList from './components/CityList';
-import CountryList from './components/CountryList';
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
 
-const BASE_URL = 'http://localhost:9000';
+const BASE_URL = "http://localhost:9000";
 
 const App = () => {
   const [cities, setCities] = useState([]);
@@ -44,9 +45,19 @@ const App = () => {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,27 @@
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>
+        position {lat} {lng}
+      </h1>
+      <button
+        onClick={() =>
+          setSearchParams({
+            lat: 23,
+            lng: 25,
+          })
+        }
+      >
+        Change position
+      </button>
+    </div>
+  );
 };
 
 export default Map;
