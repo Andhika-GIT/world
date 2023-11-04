f291ad76f230115234c4d7470656ce99e1c1b629
Andhika-GIT <andhikapr081@gmail.com>
2023-11-04 19:51:41 +0700

    feat: setting map position with geoLocation custom hooks

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,19 @@
+import { useState, useEffect } from 'react';
 import styles from './Map.module.css';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvent } from 'react-leaflet';
 import cityStore from '../stores/cityStore';
 
-import { useState, useEffect } from 'react';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
+
+// to render flag
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join('');
+  return <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />;
+};
 
 // create our own custom center, to make the map reactive when location is changed
 const ChangeCenter = ({ position }) => {
@@ -29,13 +39,7 @@ const Map = () => {
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const navigate = useNavigate();
   const { cities } = cityStore();
-
-  const flagemojiToPNG = (flag) => {
-    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-      .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-      .join('');
-    return <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />;
-  };
+  const { isLoading: geoIsLoading, position: geoPosition, getPosition } = useGeolocation();
 
   useEffect(() => {
     if (mapLat && mapLng) {
@@ -43,8 +47,18 @@ const Map = () => {
     }
   }, [mapLat, mapLng]);
 
+  useEffect(() => {
+    if (geoPosition) {
+      const { lat, lng } = geoPosition;
+      setMapPosition([lat, lng]);
+    }
+  }, [geoPosition]);
+
   return (
     <div className={styles.mapContainer}>
+      <Button type="position" onClick={getPosition}>
+        {geoIsLoading ? 'loading...' : 'use your position'}
+      </Button>
       <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
         {cities.map((city) => (
